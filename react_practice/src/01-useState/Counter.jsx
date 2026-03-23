0b0da45b38dbc966e67cc279feb41ddac15d78b7
Sudarshan <sudu1008@gmail.com>
2026-03-23 15:37:05 +0530

    Updated decrement couter

--- react_practice/src/01-useState/Counter.jsx
+++ react_practice/src/01-useState/Counter.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react'
 
-
-
-
 function Counter() {
     const [count, setCount] = useState(0)
   return (
     <div>
          <h1>{count}</h1>
-      <button onClick={()=>setCount(count-1)}>decrease</button>
+      <button onClick={()=>setCount(count > 0 ? count-1 : 0)}>decrease</button>
       <button onClick={()=>setCount(count+1)}>Increase</button>
       <button onClick={()=>setCount(0)}>Reset</button>
     </div>
   )
 }
-
 export default Counter
